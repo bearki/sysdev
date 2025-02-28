bef5d539a29490dfdb4006df6b9189584f85f229
bearki <bearki@126.com>
2025-02-28 19:25:08 +0800

    完成linux开发

--- scripts/build_linux_all.sh
+++ scripts/build_linux_all.sh
@@ -21,9 +21,9 @@ $scriptDir/build_linux_gnu_any.sh "aarch64" "$BuildVersion" "${Toolchain}"
 # # [GCC-10.3 aarch64] 交叉编译
 # Toolchain="$HOME/build-tools/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu"
 # $scriptDir/build_linux_gnu_any.sh "aarch64" "$BuildVersion" "${Toolchain}"
-# # [GCC-10.3 i686] 交叉编译
-# Toolchain="$HOME/build-tools/x86-i686--glibc--stable-2021.11-1"
-# $scriptDir/build_linux_gnu_any.sh "i686" "$BuildVersion" "${Toolchain}" "i686-buildroot-linux-gnu"
-# # [GCC-10.3 x86_64] 交叉编译
-# Toolchain="$HOME/build-tools/x86-64--glibc--stable-2021.11-5"
-# $scriptDir/build_linux_gnu_any.sh "x86_64" "$BuildVersion" "${Toolchain}" "x86_64-buildroot-linux-gnu"
+# [GCC-10.3 i686] 交叉编译
+Toolchain="$HOME/build-tools/x86-i686--glibc--stable-2021.11-1"
+$scriptDir/build_linux_gnu_any.sh "i686" "$BuildVersion" "${Toolchain}" "i686-buildroot-linux-gnu"
+# [GCC-10.3 x86_64] 交叉编译
+Toolchain="$HOME/build-tools/x86-64--glibc--stable-2021.11-5"
+$scriptDir/build_linux_gnu_any.sh "x86_64" "$BuildVersion" "${Toolchain}" "x86_64-buildroot-linux-gnu"
